39bb1d4d42893d1e9f2d2db75721fd62727a42a1
Charles Bousseau <cbousseau@anaconda.com>
2022-07-15 17:14:28 -0400

    repack libboost + start repack arrow

--- recipe/repack.sh
+++ recipe/repack.sh
@@ -14,5 +14,29 @@ src="$SRC_DIR/$PKG_NAME"
 
 cp -rv "$src"/* "$PREFIX/"
 
+# repack dependencies of omniscidbe
+if [ "$PKG_NAME" = "omniscidbe" ]; then
+    # show current rpath
+    readelf -d "$PREFIX/"lib/libDBEngine.so | egrep "RPATH|RUNPATH"
+
+    # add repack folder to rpath
+    mkdir -p "$PREFIX/omniscidb-repack/"
+    patchelf --force-rpath --add-rpath '$ORIGIN/../../../omniscidb-repack/' "$PREFIX/"lib/libDBEngine.so
+
+    # show updated rpath
+    readelf -d "$PREFIX/"lib/libDBEngine.so | egrep "RPATH|RUNPATH"
+
+    # repack boost-cpp
+    cp -rv "$SRC_DIR/boost"/lib/libboost*.so.* "$PREFIX/omniscidb-repack/"
+
+    # repack icu (needed for boost-cpp)
+    ls -R "$SRC_DIR/icu"
+    cp -rv "$SRC_DIR/icu"/lib/libicu*.so.* "$PREFIX/omniscidb-repack/"
+
+    # repack arrow-cpp
+    ls -R "$SRC_DIR/arrowcpp"
+    cp -rv "$SRC_DIR/arrowcpp"/lib/lib*.so.* "$PREFIX/omniscidb-repack/"
+fi
+
 # replace old info folder with our new regenerated one
 rm -rf "$PREFIX/info"
