50e72c1e8e4d1ccaa96fb4598ab7bdf6407dcad0
Charles Bousseau <cbousseau@anaconda.com>
2022-07-20 21:47:02 -0400

    use pyarrow 6 repack

--- recipe/repack.sh
+++ recipe/repack.sh
@@ -23,23 +23,37 @@ if [[ "${target_platform}" == "linux-64" ]]; then
         readelf -d $TARGET_SO | egrep "RPATH|RUNPATH"
 
         # add repack folder to rpath
-        mkdir -p "$PREFIX/omniscidb-repack/"
-        patchelf --force-rpath --add-rpath '$ORIGIN/../../../omniscidb-repack/' $TARGET_SO
+        mkdir -p "$PREFIX/omniscidb-repack/bin/"
+        mkdir -p "$PREFIX/omniscidb-repack/lib/"
+        patchelf --force-rpath --add-rpath '$ORIGIN/../../../omniscidb-repack/lib/' $TARGET_SO
 
         # show updated rpath
         readelf -d $TARGET_SO | egrep "RPATH|RUNPATH"
 
         # repack libclang-cpp
-        cp -rv "$SRC_DIR/libclang-cpp"/lib/libclang-cpp.so.* "$PREFIX/omniscidb-repack/"
+        cp -rv "$SRC_DIR/libclang-cpp"/lib/libclang-cpp.so.* "$PREFIX/omniscidb-repack/lib/"
 
         # repack boost-cpp
-        cp -rv "$SRC_DIR/boost"/lib/libboost*.so.* "$PREFIX/omniscidb-repack/"
+        cp -rv "$SRC_DIR/boost"/lib/libboost*.so.* "$PREFIX/omniscidb-repack/lib/"
 
         # repack icu (needed for boost-cpp)
-        cp -rv "$SRC_DIR/icu"/lib/libicu*.so.* "$PREFIX/omniscidb-repack/"
+        cp -rv "$SRC_DIR/icu"/lib/libicu*.so.* "$PREFIX/omniscidb-repack/lib/"
 
         # repack arrow-cpp
-        cp -rv "$SRC_DIR/arrowcpp"/lib/lib*.so.* "$PREFIX/omniscidb-repack/"
+        cp -rv "$SRC_DIR/arrowcpp"/lib/lib*.so.* "$PREFIX/omniscidb-repack/lib/"
+
+        if [[ "$PKG_NAME" = "pyomniscidbe" ]]; then
+            # repack pyarrow and rename it to pyarrow6 to avoid conflicts with other pyarrow versions
+            mv "$SRC_DIR/pyarrow/lib/python$PY_VER/site-packages/pyarrow" "${SP_DIR}/pyarrow6"
+            pushd "${SP_DIR}/pyarrow6"
+            patchelf --force-rpath --add-rpath '$ORIGIN/../../../../omniscidb-repack/lib/' plasma-store-server
+            for f in *.so;
+            do
+                readelf -d $f | egrep "RPATH|RUNPATH"
+                patchelf --force-rpath --add-rpath '$ORIGIN/../../../../omniscidb-repack/lib/' $f
+            done
+            popd
+        fi
 
         # for libarchive 3.5.2 conda-forge has interface wrongly set to 13
         patchelf --replace-needed "libarchive.so.13" "libarchive.so.18" $TARGET_SO
@@ -54,6 +68,19 @@ if [[ "${target_platform}" == "linux-64" ]]; then
         #     patchelf --replace-needed "$f" "REPACK_$f" $TARGET_SO
         # done
         # popd
+    fi
+
+    if [[ "$PKG_NAME" = "pyomniscidbe" ]]; then
+        # source is there to provide the tests of pyomniscidbe.
+        # Since pyarrow is repacked to pyarrow6, the tests must be updated to import pyarrow6.
+        pushd "$SRC_DIR/source/Embedded/test/"
+        for f in *.py;
+        do
+            sed -i 's/^from pyarrow /from pyarrow6 /g' $f
+            sed -i 's/^import pyarrow as/import pyarrow6 as/g' $f
+            sed -i 's/^import pyarrow$/import pyarrow6 as pyarrow/g' $f
+        done
+        popd
     fi
 fi
 
