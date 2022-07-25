d3cd3c28b81bb46fc34f676c94c962e4f91097e4
Charles Bousseau <cbousseau@anaconda.com>
2022-07-25 18:03:54 -0400

    add run_constrained pyarrow ==6.0.1 - comment test_exceptions

--- recipe/repack.sh
+++ recipe/repack.sh
@@ -38,7 +38,7 @@ if [[ "${target_platform}" == "linux-64" ]]; then
         # repack boost-cpp
         cp -rv "$SRC_DIR/boost"/lib/libboost*.so.* "$PREFIX/omniscidb-repack/lib/"
 
-        # # repack icu (needed for boost-cpp)
+        # # repack icu (needed for boost-cpp) --> not anymore after building boost-cpp 1.74 with default.
         # cp -rv "$SRC_DIR/icu"/lib/libicu*.so.* "$PREFIX/omniscidb-repack/lib/"
 
         # repack arrow-cpp
@@ -46,8 +46,8 @@ if [[ "${target_platform}" == "linux-64" ]]; then
 
         if [[ "$PKG_NAME" = "pyomniscidbe" ]]; then
             # repack pyarrow and rename it to pyarrow6 to avoid conflicts with other pyarrow versions
-            mv "$SRC_DIR/pyarrow/lib/python$PY_VER/site-packages/pyarrow" "${SP_DIR}/pyarrow6"
-            pushd "${SP_DIR}/pyarrow6"
+            mv "$SRC_DIR/pyarrow/lib/python$PY_VER/site-packages/pyarrow" "${SP_DIR}/pyarrow"
+            pushd "${SP_DIR}/pyarrow"
             patchelf --force-rpath --add-rpath '$ORIGIN/../../../../omniscidb-repack/lib/' plasma-store-server
             patchelf --force-rpath --add-rpath '$ORIGIN/../../omniscidb-repack/lib/' plasma-store-server
             for f in *.so;
@@ -62,30 +62,20 @@ if [[ "${target_platform}" == "linux-64" ]]; then
         # for libarchive 3.5.2 conda-forge has interface wrongly set to 13
         patchelf --replace-needed "libarchive.so.13" "libarchive.so.18" $TARGET_SO
 
-        # prefix repacked sonames with REPACK_ to ensure the repack deps are actually used
-        # pushd "$PREFIX/omniscidb-repack/"
-        # regex='\[(.*)\]'
-        # for f in *;
-        # do
-        #     mv -v -- "$f" "REPACK_$f"
-        #     patchelf --set-soname "REPACK_$f" "REPACK_$f"
-        #     patchelf --replace-needed "$f" "REPACK_$f" $TARGET_SO
-        # done
-        # popd
     fi
 
-    if [[ "$PKG_NAME" = "pyomniscidbe" ]]; then
-        # source is there to provide the tests of pyomniscidbe.
-        # Since pyarrow is repacked to pyarrow6, the tests must be updated to import pyarrow6.
-        pushd "$SRC_DIR/source/Embedded/test/"
-        for f in *.py;
-        do
-            sed -i 's/^from pyarrow /from pyarrow6 /g' $f
-            sed -i 's/^import pyarrow as/import pyarrow6 as/g' $f
-            sed -i 's/^import pyarrow$/import pyarrow6 as pyarrow/g' $f
-        done
-        popd
-    fi
+    # if [[ "$PKG_NAME" = "pyomniscidbe" ]]; then
+    #     # source is there to provide the tests of pyomniscidbe.
+    #     # Since pyarrow is repacked to pyarrow6, the tests must be updated to import pyarrow6.
+    #     pushd "$SRC_DIR/source/Embedded/test/"
+    #     for f in *.py;
+    #     do
+    #         sed -i 's/^from pyarrow /from pyarrow6 /g' $f
+    #         sed -i 's/^import pyarrow as/import pyarrow6 as/g' $f
+    #         sed -i 's/^import pyarrow$/import pyarrow6 as pyarrow/g' $f
+    #     done
+    #     popd
+    # fi
 fi
 
 # replace old info folder with our new regenerated one
